c1c12501154a6456248a4487d67daeed3d20f480
Amos Endersen <amenders@iupui.edu>
2018-04-24 15:00:05 -0400

    added csv output with initial data. script now functional

--- fisher.py
+++ fisher.py
@@ -12,13 +12,21 @@ else:
     ssl._create_default_https_context = _create_unverified_https_context
 
 # using username and password
-github = Github('jbcampbe', 'J3susmy1')
+g = Github('jbcampbe', 'J3susmy1')
 
 # create dataframe
-data = pd.DataFrame(columns=['name', 'actionscript', 'c', 'c#', 'c++', 'clojure', 'coffeescript', 'css', 'go', 'haskell', 'html', 'java', 'javascript', 'lua', 'matlab', 'objectivec', 'perl', 'php', 'python', 'r', 'ruby', 'scala', 'shell', 'swift', 'tex', 'vim'])
+data = pd.DataFrame(columns=['name', 'ActionScript', 'C', 'C#', 'C++', 'Clojure', 'CoffeeScript', 'CSS', 'Go', 'Haskell', 'HTML', 'Java', 'JavaScript', 'Lua', 'Matlab', 'Objective-C', 'Perl', 'PHP', 'Python', 'R', 'Ruby', 'Scala', 'Shell', 'Swift', 'TeX', 'Vim script'])
 
 # Then play with your Github objects:
-for user in github.get_users():
+for user in g.get_users():
+    userRecord = [user.login, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     for repo in user.get_repos():
-        data.loc[len(data)] = [user.name]
-        print(data)
+        for i in range(1, 26):
+            if repo.language == data.columns[i]:
+                userRecord[i] += 1
+
+    print(userRecord)
+    data.loc[len(data)] = (userRecord)
+    data.to_csv('/Users/Amos/Desktop/temp_github.csv')
+
+data.to_csv = ('/Users/Amos/Desktop/github.csv')
